a05680d95f3e95c303747144d3a1e920abd98c06
Austin Grandt <austin.grandt@gmail.com>
2020-02-18 20:30:29 -0600

    footer and other small fixes

--- config/urls.py
+++ config/urls.py
@@ -8,14 +8,9 @@ from django.views import defaults as default_views
 
 urlpatterns = [
     path("", include("app.urls")),
-    path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
-    path("users/", include("bernie_calc.users.urls", namespace="users")),
-    path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
